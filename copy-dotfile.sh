5a2af297d45aaad63a76d3e78b38328035c4d68c
王增迪 <wangzengdi@meituan.com>
2017-11-02 12:32:29 +0800

    fix iterate shell array items

--- copy-dotfile.sh
+++ copy-dotfile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-dotfiles=(".zshrc" ".bashrc" ".spacemacs.d/")
-for file in $dotfiles
+dotfiles=(".zshrc" ".bashrc" ".spacemacs.d")
+for file in ${dotfiles[@]}
 do
     cp -rf "$HOME/$file" "./$file"
 done
